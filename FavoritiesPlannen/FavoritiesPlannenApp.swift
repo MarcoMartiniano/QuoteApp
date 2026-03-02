b1235ca0925dc514599af8857a13150d22080282
MarcoMartiniano <marrco.programacao@gmail.com>
2026-03-02 11:11:25 +0100

    Initial Commit

---
+++ FavoritiesPlannen/FavoritiesPlannenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritiesPlannenApp.swift
+//  FavoritiesPlannen
+//
+//  Created by Marco Antonio Martiniano on 02.03.26.
+//
+
+import SwiftUI
+
+@main
+struct FavoritiesPlannenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
